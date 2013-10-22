42199c26e14bdd27840421e18ec6f5a37a2b353d
kyungjae.heo <kyungjae.heo@lgepartner.com>
2013-10-22 16:56:11 +0900

    HelloGitHub.java Commit!

---
+++ src/hkjude/application/github/HelloGitHub.java
@@ -0,0 +1,13 @@
+package hkjude.application.github;
+
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub!");
+
+	}
+
+}
